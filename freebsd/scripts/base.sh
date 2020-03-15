408b57258c26b8d44a01b78bd61dd42e9a120ddb
Brian Downs <brian.downs@gmail.com>
2020-03-15 14:38:07 -0700

    final

--- freebsd/scripts/base.sh
+++ freebsd/scripts/base.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+hostname freebsd01
+
 freebsd-update --not-running-from-cron fetch install || :
 
 echo security.bsd.see_other_uids=0 >> /etc/sysctl.conf
@@ -12,7 +14,6 @@ echo clear_tmp_enable=YES >> /etc/rc.conf
 echo sendmail_enable=NONE >> /etc/rc.conf
 echo icmp_drop_redirect=YES >> /etc/rc.conf
 echo tcp_drop_synfin=YES >> /etc/rc.conf
-echo denyhosts_enable=YES >> /etc/rc.conf
 echo syslogd_flags=\"-c\" >> /etc/rc.conf
 echo daily_rkhunter_update_enable=YES >> /etc/rc.conf
 echo daily_rkhunter_update_flags=\"--update --nocolors\" >> /etc/rc.conf
@@ -27,7 +28,7 @@ env ASSUME_ALWAYS_YES=YES pkg bootstrap
 pkg update
 pkg upgrade -y
 
-pkg install -y rkhunter vim wget sudo unzip nmap git curl rsync zsh
+pkg install -y rkhunter vim wget sudo unzip nmap git curl rsync zsh lsof
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
